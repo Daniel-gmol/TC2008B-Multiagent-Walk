f237a2e17c3f28c6fc9f582a24d8d708b56802a9
Daniel-gmol <117100281+Daniel-gmol@users.noreply.github.com>
2024-08-17 19:58:29 -0600

    Update agents_test.py

--- agents_test.py
+++ agents_test.py
@@ -1,60 +1,136 @@
+# Model design
 import agentpy as ap
+import numpy as np
+
+# Random
 import random
+
+# Visualization
+import matplotlib.pyplot as plt
+import IPython
+
 
 class Pedestrian(ap.Agent):
     def setup(self):
-        pass # No setup needed
+        self.velocity = [1, 0, 0]
+        self.semaphore_state = None
 
-    def move(self, grid, goal_position):
-        # Get current position
-        x, y = grid.positions[self]
+    def receive_semaphore_state(self, state):
+        self.semaphore_state = state
 
-        # Check if the car has reached the goal
-        if (x, y) == goal_position:
-            print("Reached the goal at position:", goal_position)
-            return  # Stop moving
+    def setup_pos(self, space):
+        self.space = space
+        self.neighbors = space.neighbors
+        self.pos = space.positions[self]
 
-        # Calculate new position
-        if x < grid.shape[0] - 1:  # Check if not at the rightmost edge
-            new_position = (x + 1, y)
-        else:
-            return # Stop at the end of the grid
-        
-        # Remove agent from current position
-        grid.remove_agents(self)
-        
-        # Add agent to new position
-        grid.add_agents([self], positions=[new_position])
+    def update_velocity(self):
+        if self.semaphore_state == 1:  # Green light
+            self.velocity = [0, 0, 0]  # Stop
+        elif self.semaphore_state == 0  :  # Red light
+            self.velocity = [1, 0, 0] 
 
+    def update_position(self):
+        self.space.move_by(self, self.velocity)
+
+
+class Semaphore(ap.Agent):
+    def setup(self):
+        self.state = 0  # 0 - red, 1 - green
+
+    def setup_pos(self, space):
+        self.space = space
+        self.neighbors = space.neighbors
+        self.pos = space.positions[self]
+
+    def set_state(self):
+        self.state = 1 if self.state == 0 else 0
+
+    def get_state(self):
+        return self.state
+
+    def communicate_state(self):
+        # Get all agents within 2 units of the semaphore
+        nearby_pedestrians = self.space.neighbors(self, distance=2)
+
+        # Communicate the state to nearby pedestrians
+        for pedestrian in nearby_pedestrians:
+            if isinstance(pedestrian, Pedestrian):
+                pedestrian.receive_semaphore_state(self.state)
 
 
 class Model(ap.Model):
     def setup(self):
         # Create space
-        self.space = ap.Space(self, shape=[self.p.road_size]*self.p.ndim, torus=False) # torus true to return to the start
-        
+        self.space = ap.Space(
+            self, shape=[self.p.space_size]*self.p.ndim, torus=False)
+
         # Create Pedestrian agents
         self.pedestrianAgents = ap.AgentList(self, self.p.n_people, Pedestrian)
 
-        # Create random positons and put agents in space
-        for agent in self.pedestrianAgents:
-            x_position = random.uniform(0, 2)
-            position = [x_position, 0, 0]  # z:0, y:0, x between 0 and 2
-            self.space.add_agents(agent, position)
+        # Create Semaphore agent
+        self.semaphore = ap.AgentList(self, 1, Semaphore)
 
-    
-    
+        # Create random positons according to sidewalk size and agent inbetween space
+        available_x_pos = np.arange(
+            0, self.p.sidewalk_size, self.p.ag_x_distance)
+        available_y_pos = np.arange(
+            0, self.p.sidewalk_size, self.p.ag_y_distance)
+        available_positions = [(x, y, 0)
+                               for x in available_x_pos for y in available_y_pos]
+
+        # Randomly select positions for agents
+        ag_pos = random.sample(available_positions, self.p.n_people)
+        self.space.add_agents(self.pedestrianAgents, ag_pos)
+
+        # Add semaphore to space
+        self.space.add_agents(self.semaphore, [(30, 5, 0)])
+
+        # Set up semaphore position
+        self.semaphore.setup_pos(self.space)
+
+        # Set up agents positions
+        self.pedestrianAgents.setup_pos(self.space)
+
     def step(self):
-        # Move each car in each step (there is only 1 in this example)
-        for pedestrian in self.pedestrianAgents:
-            # Position before step
-            print("\nThis is my grid postion before: ", self.grid.positions[pedestrian])
-            
-            # Step: Move the car
-            pedestrian.move(self.grid, self.goal_position)
-            
-            # Position after step
-            print("This is my grid postion after: ", self.grid.positions[pedestrian])
+        # Toggle the semaphore state every 5 steps
+        if self.t % 8 == 0:
+            self.semaphore.set_state()
+        
+        # Semaphore communicates its state to nearby pedestrians
+        self.semaphore.communicate_state()
+
+        # Pedestrians update their velocities based on the semaphore state
+        self.pedestrianAgents.update_velocity()
+
+        # Pedestrians move based on their updated velocities
+        self.pedestrianAgents.update_position()
+
+
+def animation_plot_single(m, ax):
+    ndim = m.p.ndim
+    ax.set_title(f"Model {ndim}D t={m.t}")
+    pos = m.space.positions.values()
+    pos = np.array(list(pos)).T  # Transform
+    x = m.semaphore.get_state()
+    if x[0] == 0:
+        colors = ['black', 'black', 'black', 'black', 'black', 'red']
+    else:
+        colors = ['black', 'black', 'black', 'black', 'black', 'green']
+    ax.scatter(*pos, s=20, c=colors)
+    # print("Positions: ", *pos)
+    ax.set_xlim(0, m.p.space_size)
+    ax.set_ylim(0, m.p.space_size)
+    if ndim == 3:
+        ax.set_zlim(0, m.p.space_size)
+    ax.grid(True)
+
+
+def animation_plot(m, p):
+    projection = '3d' if p['ndim'] == 3 else None
+    fig = plt.figure(figsize=(7, 7))
+    ax = fig.add_subplot(111, projection=projection)
+    animation = ap.animate(m(p), fig, ax, animation_plot_single)
+    return IPython.display.HTML(animation.to_jshtml(fps=20))
 
 
 # Start
@@ -62,19 +138,16 @@ print("Starting ...")
 
 # Set up parameters
 parameters = {
-    'steps': 4,  # Number of simulation steps
-    'seed' : 15, # Seed for random number generator
+    'steps': 50,  # Number of simulation steps
 
-    'n_people': 15,  # Number of cars
-    'road_size': 100, # 1 metro por unidad
-    'ndim': 3, # 3D space
+    'space_size': 50,  # 1 metro por unidad
+    'ndim': 3,  # 3D space
+
+    'n_people': 5,  # Number of people
+    'sidewalk_size': 10,  # 1 metro por unidad
+    # maybe genrate randomly for each agent likes but initally have a random distance between 0.5 and 1.5
+    'ag_x_distance': 0.7,
+    'ag_y_distance': 1,
 }
 
-model = Model(parameters) # Create the model
-results = model.run()   # Run the simulation
-
-# Access the final positions of the cars (Only 1 car in this example)
-for i, car in enumerate(model.pedestrianAgents):
-    x, y = model.grid.positions[car]
-    print(f'Car {i} final position: ({x}, {y})')
-
+animation_plot(Model, parameters)
